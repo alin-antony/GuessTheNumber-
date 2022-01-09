d0dd19474865468045866039f2a254b8c5723bb0
Alin asha.A <89380668+alin-antony@users.noreply.github.com>
2022-01-10 00:53:57 +0530

    Update GuessTheNumber .java

--- GuessTheNumber .java
+++ GuessTheNumber .java
@@ -6,35 +6,32 @@ public class GuessTheNumber {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a positive number below 30:");
         int Number = sc.nextInt();
-            int count = 1;
+        int count = 1;
         while (Number != range) {
             count++;
             if (Number < range)
                 System.out.println(Number + " is smaller than the guessed number!!!");
             else
                 System.out.println(Number + " is greater than the guessed number!!!");
-            System.out.println("Enter another number: ");
+
+            if (count == 5){
+                System.out.println("***********************************************");
+                System.out.println("\nYou lost your chances,Better luck next time!!!");
+            System.exit(0);}
+
+            System.out.println("enter another number:");
             Number = sc.nextInt();
 
-            if(count>5){
-                System.out.println("better luck next time!!");
-                System.exit(0);
-            }
-            }
+        }
         if (count == 1)
-                    System.out.println("Brillient!!\ncongratulation");
-                else if (count == 2)
-                    System.out.println("good job!!\ncongratulation");
-                else if (count == 3)
-                    System.out.println("good try!!\ncongratulation");
-                else if (count == 4)
-                    System.out.println("you win!!\ncongratulation");
-        if (count ==5)
-            System.out.println("you win!!\ncongratulation");
-           }
-           }
-
-
-
+                System.out.println("Brillient!!\ncongratulation");
+            else if (count == 2)
+                System.out.println("good job!!\ncongratulation");
+            else if (count == 3)
+                System.out.println("good try!!\ncongratulation");
+            else if (count == 4)
+                System.out.println("you win!!\ncongratulation");
+    }
+    }
 
 
