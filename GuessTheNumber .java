4d31c271fa690a8f92b8a5788f4f75c87b41b3ab
Alin asha.A <89380668+alin-antony@users.noreply.github.com>
2022-01-09 23:58:37 +0530

    Update GuessTheNumber .java

--- GuessTheNumber .java
+++ GuessTheNumber .java
@@ -1,5 +1,4 @@
 //finding the number guessed by the computer//
-
 import java.util.Scanner;
 public class GuessTheNumber {
     public static void main(String[] args) {
@@ -7,8 +6,7 @@ public class GuessTheNumber {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a positive number below 30:");
         int Number = sc.nextInt();
-
-        int count = 1;
+            int count = 1;
         while (Number != range) {
             count++;
             if (Number < range)
@@ -17,12 +15,12 @@ public class GuessTheNumber {
                 System.out.println(Number + " is greater than the guessed number!!!");
             System.out.println("Enter another number: ");
             Number = sc.nextInt();
-            if (count >= 5) {
+
+            if(count>5){
                 System.out.println("better luck next time!!");
                 System.exit(0);
             }
-
-        }
+            }
         if (count == 1)
                     System.out.println("Brillient!!\ncongratulation");
                 else if (count == 2)
@@ -31,6 +29,12 @@ public class GuessTheNumber {
                     System.out.println("good try!!\ncongratulation");
                 else if (count == 4)
                     System.out.println("you win!!\ncongratulation");
+        if (count ==5)
+            System.out.println("you win!!\ncongratulation");
+           }
+           }
 
-    }
-        }
+
+
+
+
